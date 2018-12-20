761adb8522f139d1ef3cd1eee6a14f19dbdbb99e
Jan Martinek <info@jan-martinek.com>
2018-12-20 16:19:55 +0100

    Quotes

--- static/scripts.js
+++ static/scripts.js
@@ -1,13 +1,53 @@
+/* globals quotes, markerData, window, document */
 document.addEventListener("DOMContentLoaded", function(event) {
   document.body.classList.add("js");
 
   if (document.getElementById('map-brno')) initMap();
+
+  if (document.getElementById('quote-wrapper')) {
+    var generateQuote = initQuoteGenerator();
+   
+    window.setInterval(function() {
+      var q = generateQuote();
+      document.getElementById('quote-wrapper').innerHTML = '<blockquote>' + q.quote + '<br><cite>' + q.author + '</cite></blockquote>';
+    }, 1000 * 3);
+
+  }
+
   Array.prototype.slice.call(document.querySelectorAll("dt")).forEach(function(el) {
     el.addEventListener('click', function(e) {
       el.nextElementSibling.classList.toggle("visible");
     });
   });
 });
+
+
+function initQuoteGenerator() {
+  var qGen = new markov(quotes.map(function(q) {return q.quote}).join(" ").toLowerCase().replace(/\./g, ' '), /[AÁBCČDĎEÉĚFGHChIÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽaábcčdďeéěfghchiíjklmnňoópqrřsštťuúůvwxyýzž']+/g);
+  var aGen = new markov(quotes.map(function(q) {return q.author}).join(" ").toLowerCase(), /./g);
+
+  return function() {
+    if (Math.random() > 0.5) {
+      return quotes[Math.floor(Math.random()*quotes.length)];
+    } else {
+      return {
+        quote: formatQuote(qGen.gen(Math.ceil(5 + 15 * Math.random()))),
+        author: formatAuthor(aGen.gen(Math.ceil(10 + 15 * Math.random())))
+      }
+    }
+  }
+}
+
+function formatAuthor(n) {
+  return toTitleCase(n.replace(/\|/g, ''));
+}
+
+function formatQuote(q) {
+  q = q.replace(/\|/g, ' ').trim();
+  q = q[0].toUpperCase() + q.slice(1);
+  if (q[length - 1] !== '.') q += '.';
+  return q;
+}
 
 function initMap() {
   var map = L.map('map-brno', {
@@ -59,4 +99,77 @@ function groupClick(event) {
     if (prev) prev.classList.remove('active');
     el.classList.add('active');
   }
+}
+
+
+// simplified from https://github.com/BrianHicks/Markov-Generator/
+function markov(input, reg) {
+  this.data = {};
+  s = input.match(reg);
+  for (var i = 0; i < s.length-1; i++) {
+    if(s[i] in this.data) {
+      if (s[i+1] in this.data[s[i]]) {
+        this.data[s[i]][s[i+1]]++;
+      } else {
+        this.data[s[i]][s[i+1]] = 1;
+      }
+    } else {
+      this.data[s[i]] = new Object();
+      this.data[s[i]][s[i+1]] = 1;
+    }
+  }
+  
+  this.gen = function(l) {
+    var sanitycheck = false;
+    var out = new Array();
+    while (sanitycheck == false) {
+      sanitycheck = true;
+      var rProperty = findRandomProperty(this.data);
+      var rList = expand(rProperty);
+      var l1 = rList.length;
+      out[0] = rList[Math.round(Math.random() * l1)];
+      if (typeof out[0] == "undefined") { sanitycheck = false; }
+      if (sanitycheck) {
+        for (var i = 0; i < l-1; i++) {
+          var usableLength = expand(this.data[out[i]]).length-1;
+          var randomInt = Math.round(Math.random() * usableLength);
+          var nextLetter = expand(this.data[out[i]])[randomInt];
+          out.push(nextLetter);
+        }
+      }
+    }
+
+    return out.join("|");
+  }
+  
+  var findRandomProperty = function(o) {
+    l1 = 0;
+    for (i in o) {
+      l1++;
+    }
+    var r1 = Math.round(Math.random() * l1);
+    l2 = 0;
+    for (i in o) {
+      l2++;
+      if (l2 == r1) {
+        return o[i];
+      }
+    }
+  }
+  
+  var expand = function(obj) {
+    oArray = new Array();
+    for (var prop in obj) {
+      for (var i = 0; i < obj[prop]; i++) {
+        oArray.push(prop);
+      }
+    }
+    return oArray;
+  }
+}
+
+function toTitleCase(str) {
+  return str.replace( /\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
